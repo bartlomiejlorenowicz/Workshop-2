9127c60f24b2317fdc2bfffaf9453a9158ed8281
Bartlomiej Lorenowicz <bartlomiejlorenowicz>
2023-12-20 22:07:05 +0100

    CRUD app v2

--- src/main/java/pl/coderslab/entity/UserDao.java
+++ src/main/java/pl/coderslab/entity/UserDao.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
 
 public class UserDao {
 
@@ -15,6 +16,17 @@ public class UserDao {
 
     private static final String SELECT_USER_BY_ID =
             "SELECT * from users WHERE users.id = ?";
+
+    private static final String UPDATE_USER_NAME =
+            "UPDATE users SET username = ? WHERE id = ?";
+
+    private static final String DELETE_USER_BY_ID =
+    "DELETE FROM users WHERE id = ?";
+
+    private static final String SELECT_ALL_USERS =
+            "SELECT * FROM users";
+
+    private static User[] users = new User[0];
 
 
     public User create(User user) {
@@ -58,7 +70,47 @@ public class UserDao {
     }
 
     public void update(User user) {
-
+        try (Connection connection = DBUtil.getConnection(); PreparedStatement statement = connection.prepareStatement(UPDATE_USER_NAME)) {
+            statement.setString(1, user.getUserName());
+            statement.setInt(2, user.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
+    public void delete(int userId) {
+        try (Connection connection = DBUtil.getConnection(); PreparedStatement statement = connection.prepareStatement(DELETE_USER_BY_ID)) {
+            statement.setInt(1, userId);
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public User[] findAll() {
+        try(Connection connection = DBUtil.getConnection(); PreparedStatement statement = connection.prepareStatement(SELECT_ALL_USERS)) {
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setUserName(rs.getString("username"));
+                user.setEmail(rs.getString("email"));
+                users = addToArray(user, UserDao.users);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return new User[0];
+        }
+        return users;
+    }
+
+    private User[] addToArray(User u, User[] users) {
+        User[] tmpUsers = Arrays.copyOf(users, users.length + 1); // Tworzymy kopię tablicy powiększoną o 1.
+        tmpUsers[users.length] = u; // Dodajemy obiekt na ostatniej pozycji.
+        return tmpUsers; // Zwracamy nową tablicę.
+    }
+
 }
 
